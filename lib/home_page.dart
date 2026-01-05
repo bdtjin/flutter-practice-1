c670e6d711df974296e5929491fe598ee3097f7b
jin <bdtjin00@gmail.com>
2026-01-05 09:48:59 +0900

    feat: 상태분리 완료

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,8 +9,9 @@ import 'package:http/http.dart';
 class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
-}
 
+  // 의존성 주입!
+}
 class _HomePageState extends State<HomePage> {
   Weather? weather;
 
@@ -28,26 +29,10 @@ class _HomePageState extends State<HomePage> {
           children: [
             if (weather != null) Temp(weather: weather!), // weather 객체가 null이 아닐 때만 Temp 위젯을 표시
             if (weather != null) Speed(weather: weather!),
-            
+
             IconButton(
               onPressed: () async {
-                // 1. 데이터 달라고 편지 쓰기! -> http 통신 GET 요청
-                Client httpClient = Client();
-                Uri target = Uri.parse(
-                  "https://api.open-meteo.com/v1/forecast?latitude=37.57&longitude=126.98&current_weather=true",
-                );
-                Response r = await httpClient.get(target);
-                // 2. 정보가 담긴 답장 받기
-                print(r.statusCode); // 편지가 정상적으로 전달되고 답장을 받았는지 알려주는 숫자코드. 성공 : 200, 실패 : 404, 500...
-                print(r.body); // 답장 내용
-                // 3. 답장내용(JSON)을 Map으로 변경
-                Map<String, dynamic> jsonMap = jsonDecode(r.body);
-                // 4. Map을 객체로 변경 (3~4 : 역직렬화)
-                Weather w = Weather.fromJson(jsonMap);
-                weather = w;
-                displayTemp = "${w.currentWeather.temperature} ${w.units.temperature}";
-                setState(() {
-                });
+                
               },
               icon: Icon(Icons.refresh),
             ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_practice_1/home_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(App());
+  // ProviderScope : riverpod 패키지에서 제공하는 위젯
+  runApp(ProviderScope(child: App()));
 }
 
 class App extends StatelessWidget{
