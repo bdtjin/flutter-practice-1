7712f65b5ac9ae576b25db5ad1a42048b9a6757d
jin <bdtjin00@gmail.com>
2026-01-13 10:27:00 +0900

    WIP: 컬렉션 가져오기

--- lib/matzip_repository.dart
+++ lib/matzip_repository.dart
@@ -1,11 +1,18 @@
 
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_practice_1/matzip.dart';
 
 class MatzipRepository {
   // 파이어스토어에서 맛집 가져와서 맛집리스트로 변환해서 전달
   Future<List<Matzip>> getAllMatzip () async {
     print("getAllMatzip 호출됨");
+    // 컬렉션 모든 문서들 가져오기
+    final colRef = FirebaseFirestore.instance.collection('matzip');
+    
+    // 문서들 List -> Map
+    // 맛집리스트로 변환??
+    // 맛집리스트 반환
     return [];
   }
 }
